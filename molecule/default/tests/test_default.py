10727729d9fa942b6ecaa1d0b7beafb0a3a7a188
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2024-05-17 18:32:14 -0400

    Install the Backports version on Debian Bookworm
    Update testing to reflect the change in package source. Also adjust the
    order of tasks and their names since we now install from Backports on
    multiple Debian releases.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -16,9 +16,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_packages(host, pkg):
     """Test that the appropriate packages were installed."""
     assert host.package(pkg).is_installed
-    # Verify that the version from bullseye-backports is installed
-    if (
-        host.system_info.distribution == "debian"
-        and host.system_info.codename == "bullseye"
-    ):
-        assert host.package(pkg).version.startswith("7.93")
+    # Verify that the appropriate version from Debian Backports is installed
+    if host.system_info.distribution == "debian":
+        if host.system_info.codename == "bullseye":
+            assert host.package(pkg).version.startswith("7.93")
+        if host.system_info.codename == "bookworm":
+            assert host.package(pkg).version.startswith("7.94")
